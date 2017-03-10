0b8b620fe26a899f4186d779d59057957ba0779f
Anca Antochi <ancan@microsoft.com>
2017-03-10 11:54:53 -0800

    Java out of process module implementation

---
+++ proxy/gateway/java/gateway-remote-module/src/main/java/com/microsoft/azure/gateway/remote/CommunicationStrategy.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+package com.microsoft.azure.gateway.remote;
+
+import java.nio.ByteBuffer;
+
+import org.nanomsg.NanoLibrary;
+
+interface CommunicationStrategy {
+    RemoteMessage deserializeMessage(ByteBuffer buffer, byte version) throws MessageDeserializationException;
+    int getEndpointType(NanoLibrary nano);
+    String getEndpointUri(String identifier);
+}
---
+++ tools/build_java_oop.cmd
@@ -0,0 +1,47 @@
+@REM Copyright (c) Microsoft. All rights reserved.
+@REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+@setlocal EnableExtensions EnableDelayedExpansion
+@echo off
+
+set current-path=%~dp0
+
+rem // remove trailing slash
+set current-path=%current-path:~0,-1%
+
+set build-root=%current-path%\..
+rem // resolve to fully qualified path
+for %%i in ("%build-root%") do set build-root=%%~fi
+
+set local-install=%build-root%\install-deps
+
+set build-jnano-root=%current-path%\..\build_jnano
+
+echo Cleaning up build artifacts...
+
+rem Clear the jnano build folder so we have a fresh build
+rmdir /s/q %build-jnano-root%
+if not !ERRORLEVEL!==0 exit /b !ERRORLEVEL!
+
+mkdir %build-jnano-root%
+if not !ERRORLEVEL!==0 exit /b !ERRORLEVEL!
+
+pushd %build-jnano-root%
+
+rem Clone jnano
+git clone -b develop https://github.com/ancaantochi/jnano.git
+
+popd
+
+pushd %build-jnano-root%\jnano
+call mvn clean install -DskipTests
+if errorlevel 1 goto :eof
+popd
+
+REM -- Build Java Remote SDK --
+pushd %build-root%\proxy\gateway\java\gateway-remote-module
+call mvn clean install
+if errorlevel 1 goto :eof
+popd %build-root%
+
+:eof
